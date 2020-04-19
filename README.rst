e24ea651a2a1dd68cff63cd9ab18c28156c013bb
Rohit kumar <rgrohit81@gmail.com>
2020-04-19 20:00:50 +0530

    adding custom templates

---
+++ README.rst
@@ -0,0 +1,10 @@
+Django allauth demonstration
+****************************
+Simple example of djangoallauth package, that demonstrate how to use local resister login/password /email verification as well as
+social Oauth2 login. (e.g login with Discord and github)
+
+To setup, make sure you put the client id and secret in a model/db entry
+for the social auth apps enabled. do this through admin interface.
+
+Documentation:
+https://django-allauth.readthedocs.io/en/latest/installation.html
--- posts/views.py
+++ posts/views.py
@@ -4,20 +4,21 @@ from django.shortcuts import render
 from .forms import NameForm
 
 
+#
+# @login_required
+# def get_name(request):
+#     if request.method == 'POST':
+#         form = NameForm(request.POST)
+#         if form.is_valid():
+#             return HttpResponse('thanks..the input is valid')
+#     else:
+#         form = NameForm()
+#
+#     return render(request, 'name.html', {'form': form})
+
 @login_required
 def get_name(request):
-    if request.method == 'POST':
-        form = NameForm(request.POST)
-        if form.is_valid():
-            return HttpResponse('thanks..the input is valid')
-    else:
-        form = NameForm()
-
-    return render(request, 'name.html', {'form': form})
-
-# @login_required
-# def test(request):
-#     import pprint
-#     pprint.pprint(request.user)
-#     return HttpResponse(
-#         "<h2>Its Working User</h2>" + str(request.user) + "<h2>  This view is accessed by allauth..!!</h2>")
+    import pprint
+    pprint.pprint(request.user)
+    return HttpResponse(
+        "<h2>Its Working User</h2>" + str(request.user) + "<h2>  This view is accessed by allauth..!!</h2>")
